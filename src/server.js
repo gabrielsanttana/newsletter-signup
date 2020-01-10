96533acb0f8a53958b78ddaf745c0f91bd9ff387
Gabriel Santana <gabriel.gsantana7@gmail.com>
2020-01-10 01:26:52 -0300

    Add process.env.PORT

--- src/server.js
+++ src/server.js
@@ -51,6 +51,7 @@ app.post("/memberExists", (req, res) => {
     res.redirect("/");
 });
 
-app.listen(3000, () => {
+
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port 3000");
 });
